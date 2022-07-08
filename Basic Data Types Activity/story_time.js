8fbef593ef6dbde8c4dea22ddf5d7a2e7ac148bc
emily debley <emilydebley@gmail.com>
2022-07-08 15:05:01 -0700

    added the files

---
+++ Basic Data Types Activity/story_time.js
@@ -0,0 +1,45 @@
+// TODO: Create 4-String variables to introduce develop your story.
+// YOUR CODE GOES HERE
+var the_title_of_the_story = "Jens Does it Again";
+var the_name_of_the_global_coin_related_crisis = "coinpocalypse";
+var founder_of_the_glorious_coinstar_company = "Jens Molbak";
+var the_signature_catchphrase_of_Jens_Molbak = "Get ready for a coin'in!";
+
+
+// TODO: Create 3-String variables to set the time period of your story or discuss other number elements.
+// YOUR CODE GOES HERE
+
+ACS = 32;
+//years after the founding of the glorious coinstar company 
+
+var number_of_central_park_sized_coins_hurtling_towards_the_earth = 843;
+var number_of_hours_until_impact = 2;
+
+// TODO: Create 1-Array variable to show a collection of items your character might have.
+// YOUR CODE GOES HERE
+let primary_psychic_powers_possesed_by_Jens_Molbak = ["levitation", "telekinesis", "pyrokinesis", "coinkinesis", "prophetic foresight", "ESP", "aura reading", "empathy"];
+
+// TODO: Create 1-Boolean variable to demonstrate a true or false scenario.
+// YOUR CODE GOES HERE
+var the_world_is_in_grave_danger = true;
+
+// TODO: Print your story to the console.
+// YOUR CODE GOES HERE
+console.log(the_title_of_the_story+", by emily"+"\n");
+console.log("the year is "+ACS+" ACS");
+console.log("there are "+number_of_central_park_sized_coins_hurtling_towards_the_earth+" gaint coins expected to crash into the earth in "+number_of_hours_until_impact+" hours");
+console.log("the media has dubbed this event the "+the_name_of_the_global_coin_related_crisis);
+console.log("nasa has rapidly prepared an experiemental space shuttle to cary the earth's best, brightest, and wealthyist to safety");
+console.log("but founder of coinstar, "+founder_of_the_glorious_coinstar_company+", missed the boarding window as he was too busy fighting petty crime in the streets of Bellevue, Washington");
+console.log("he must now force his way onto the shuttle to reach outer space so the giant coins will be in range of "+primary_psychic_powers_possesed_by_Jens_Molbak[3]);
+console.log(founder_of_the_glorious_coinstar_company+" uses "+primary_psychic_powers_possesed_by_Jens_Molbak[0]+" to reach the launch pad in washionton DC");
+number_of_hours_until_impact--;
+console.log("he has "+number_of_hours_until_impact+" hour left");
+console.log("hordes of unwashed and unproductive people claw at the concrete wall guarding the launch pad, unfased by defensive gunfire from our boys in blue");
+console.log(founder_of_the_glorious_coinstar_company+" takes a few momments to help our hero's out, thining the croud with "+primary_psychic_powers_possesed_by_Jens_Molbak[2]);
+console.log(founder_of_the_glorious_coinstar_company+" then boards the shuttle with seconds to spare");
+console.log("once the shuttle reaches the outer atmosphere, "+founder_of_the_glorious_coinstar_company+' says "'+the_signature_catchphrase_of_Jens_Molbak+'" and deflects the giant coins');
+var the_world_is_in_grave_danger = false;
+console.log("another passanger asks "+founder_of_the_glorious_coinstar_company+" if the world is still in grave danger");
+console.log(founder_of_the_glorious_coinstar_company+" says this is "+the_world_is_in_grave_danger);
+
